3df55bafc6d53a3df7a0e9fb3f05ded34393f8ce
WanjikuMac <edwardscaroline32@gmail.com>
2019-01-15 22:22:26 +0300

    supervisor for the delegates server

---
+++ lib/prac/supervisor.ex
@@ -0,0 +1,15 @@
+defmodule Prac.Supervisor do
+  use Supervisor
+
+  def start_link do
+    Supervisor.start_link(__MODULE__, [])
+  end
+
+  def init([]) do
+    children = [
+      worker(Prac.Server, [])
+    ]
+
+    supervise(children, strategy: :one_for_one)
+  end
+end
