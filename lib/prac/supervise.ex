2a3479d8fb8f50920b1d634cdcce3189c091254f
WanjikuMac <edwardscaroline32@gmail.com>
2019-01-16 22:44:46 +0300

    supervisor for the child

---
+++ lib/prac/supervise.ex
@@ -0,0 +1,16 @@
+defmodule Prac.Supervise do
+
+  use Supervisor
+
+    def start_link(opts) do
+      Supervisor.start_link(__MODULE__, :ok, opts)
+  end
+
+  def init(:ok) do
+    children = [
+    {Prac.Registry, name: Prac.Registry}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
